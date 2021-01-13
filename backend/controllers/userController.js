6b23cc1a3adbc0291a0aaecd02d1048852ac46a3
nithinwebdev <nithin.thewebdev@gmail.com>
2021-01-14 01:09:08 +0530

    added update name,email,password put method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,6 +48,38 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error("Invalid Email or password");
   }
 });
+//@desc  Update user profile
+//@route PUT /api/users/profile
+//@access Private
+// this below route will require a token from the front end
+const updateUserProfile = asyncHandler(async (req, res) => {
+  //find the user by id
+  const user = await User.findById(req.user._id);
+  // res.send("Success");
+  if (user) {
+    // if the request contains an updated value in req.body use the updated value from request,
+    // if the updated value is not present in req.body use existing value
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      // password is encrypted automatically since we are using a middleware .pre('save') before saving the password
+      user.password = req.body.password;
+    }
+    // save the updated user
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      // the token will have the user's id embedded in it
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("Error-User Not Found");
+  }
+});
 //@desc  Register a new user
 //@route POST /api/users
 //@access Public
@@ -81,4 +113,4 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Error--Invalid user data");
   }
 });
-export { authUser, getUserProfile, registerUser };
+export { authUser, getUserProfile, registerUser, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from "../controllers/userController.js";
 // import middleware
 import { protect } from "../middleware/authMiddleware.js";
@@ -13,5 +14,9 @@ router.post("/login", authUser);
 router.route("/").post(registerUser);
 // we need to protect the getUserProfile route,
 //to implement the protect middleware we need to pass it first
-router.route("/profile").get(protect, getUserProfile);
+// we chain the put method for updating the user profile
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 export default router;
